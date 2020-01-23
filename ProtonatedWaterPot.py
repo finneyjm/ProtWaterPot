5c6a07d2830f9f9ad2f7cc32c53754265f51455b
finneyjm <finneyjm@uw.edu>
2020-01-23 12:31:13 -0800

    Commit initial

---
+++ ProtonatedWaterPot.py
@@ -0,0 +1,55 @@
+import os, sys
+
+
+class Potential:
+
+    has_been_loaded = False
+
+    def __init__(self, natm):
+        if self.has_been_loaded:
+            raise Exception("Can't load this dumb Bowman potential twice")
+        self._natm = natm
+        self.load_potential()
+
+    def init_potential(self):
+        cur_dir = os.getcwd()
+        try:
+            where_u_at = os.path.join(
+                os.path.dirname(__file__),
+                "f2py_prot_water_pot"
+            )
+            os.chdir(where_u_at)
+            sys.path.insert(0, where_u_at)
+
+            try:
+                import ProtWaterPot
+            except ImportError:
+                self.compile_potential(True)
+            ProtWaterPot.init_param(self._natm)
+            self._pot = ProtWaterPot.proto_potcalc
+        finally:
+            os.chdir(cur_dir)
+
+    def get_potential(self, coords):
+        """Assumes coords in (nwalkers, natm, 3)
+
+        :param coords:
+        :type coords:
+        :return:
+        :rtype:
+        """
+        return self._pot(coords.T, len(coords), self._natm)  # do the real thing
+
+    def load_potential(self):
+        self.init_potential()
+        cls = type(self)
+        cls.has_been_loaded = True
+
+    @staticmethod
+    def compile_potential(called_from_error):
+        if called_from_error:
+            os.chdir("..")
+        else:
+            os.chdir(os.path.dirname(__file__))
+        import subprocess
+        subprocess.call(["bash", "build.sh"])
---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#####################################################
+### Compiling the Bowman PES for Protanated Water ###
+#####################################################
+
+# This is specifically if you want to use this PES
+# with f2py and don't want to have to write your
+# walkers to file over and over
+
+# Compile the libraries for the 2-body interactions
+cd source_code/water_lib/2-body/pes
+./mke
+cd ../dms
+./mke
+cd ../
+
+# Move these new libraries to the lib folder for later
+cp pes/libpes2b.a dms/libdms2b.a ../../../lib/watlib
+
+# Compile the libraries for the 3-body interactions
+cd ../3-body
+make
+
+# Move this library to the lib folder
+cp -r libpes3b.a mod3b ../../../lib/watlib
+
+# Compile the libraries for the PES
+cd ../../pes_lib/inv
+make
+cd ../pes-xyz
+make
+cp *.a ..
+cd ..
+
+# Moving everything to the lib folder
+cp -r *.a mod ../../lib/pes_lib
+
+# Compile the PES and then use f2py
+cd ../../PES
+make getpot
+make -f MakefileJacob ProtWaterPot
+
+# Move the .so file to a better location
+cp *.so ../f2py_prot_water_pot
